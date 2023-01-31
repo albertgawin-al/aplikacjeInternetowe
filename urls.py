c5a515eabed702cbfd9753c165d70282654414aa
Albert Gawin <99310736+albertgawin-al@users.noreply.github.com>
2023-01-31 11:25:09 +0100

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,4 +1,4 @@
-API_KEY = "RGAPI-59d8be48-9b30-44b7-b299-81c03259ada8"
+API_KEY = ""
 
 CHAMPIONS_API_URL = "http://ddragon.leagueoflegends.com/cdn/13.1.1/data/pl_PL/champion.json"
 CHAMPION_API_URL = "http://ddragon.leagueoflegends.com/cdn/13.1.1/data/pl_PL/champion"
